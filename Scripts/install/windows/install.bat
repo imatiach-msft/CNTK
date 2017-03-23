2481d5c69427390d63b1d3997b459862ad852ee7
Wolfgang Manousek <wolfma@microsoft.com>
2017-03-23 08:40:06 +0100

    address CR comments

--- Scripts/install/windows/install.bat
+++ Scripts/install/windows/install.bat
@@ -4,11 +4,15 @@
 @REM Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 @REM
 
+@echo.
+@echo CNTK Binary Install Script
+
 setlocal
 set allArgs=%*
 set installBatchDir=%~dp0
 set psInstall=%installBatchDir%ps\install.ps1
 set PSModulePath=%PSModulePath%;%installBatchDir%ps\Modules
+
 :loop
 if /I "%~1"=="-h" goto :helpMsg
 if /I "%~1"=="/h" goto :helpMsg
@@ -20,22 +24,22 @@ shift
 
 if not "%~1"=="" goto loop
 
-powershell -NoProfile -NoLogo -Command "Get-ChildItem '%installBatchDir%' -Include *.psm1,*.ps1 -recurse -ErrorAction Stop | unblock-file -ErrorAction Stop"
-if ERRORLEVEL 1 goto errorUnblock
+powershell -NoProfile -NoLogo -Command Get-ChildItem '%installBatchDir%' -Include *.psm1,*.ps1 -recurse -ErrorAction Stop ^| unblock-file -ErrorAction Stop
+if errorlevel 1 (
+  @echo Error encountered during UNBLOCK-FILE operartion
+  endlocal
+  exit /b 1
+)
 
 powershell -NoProfile -NoLogo -ExecutionPolicy RemoteSigned %psInstall% %allArgs%
-if ERRORLEVEL 1 echo Installer reported an error!!
-goto finish
-
-
-:errorUnblock
-@echo. 
-@echo. Error encountered during UNBLOCK-FILE operartion
-goto finish
+if errorlevel 1 (
+  @echo Error encountered during install operation
+  endlocal
+  exit /b 1
+)
+goto :eof
 
 :helpMsg
-@echo.
-@echo. CNTK Binary Install Script
 @echo.
 @echo More help can be found at: 
 @echo     https://github.com/Microsoft/CNTK/wiki/Setup-Windows-Binary-Script
@@ -44,5 +48,4 @@ goto finish
 @echo     https://github.com/Microsoft/CNTK/wiki/Setup-Windows-Binary-Script-Options
 @echo.
 
-:finish
-endlocal
+
